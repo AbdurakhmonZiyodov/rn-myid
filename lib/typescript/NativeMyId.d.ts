16c5d009d8d1bf07a758c439af18f5fe637cd288
Abdurakhmon <abdurakhmonziyodov@gmail.com>
2026-04-23 15:38:41 +0500

    chore: release v1.0.5

--- lib/typescript/NativeMyId.d.ts
+++ lib/typescript/NativeMyId.d.ts
@@ -1,9 +1,8 @@
-import type { TurboModule } from 'react-native';
+import type {TurboModule} from 'react-native';
 export interface Spec extends TurboModule {
-    start(sessionId: string, clientHash: string, clientHashId: string, environment: string, locale: string): void;
-    isAvailable(): boolean;
-    addListener(eventName: string): void;
-    removeListeners(count: number): void;
+  start(sessionId: string, clientHash: string, clientHashId: string, environment: string, locale: string): void;
+  addListener(eventName: string): void;
+  removeListeners(count: number): void;
 }
 declare const _default: Spec | null;
 export default _default;
